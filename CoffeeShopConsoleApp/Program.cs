ce56cceb274eb8d703f625d752edae4fd0c43e07
sarahBæk2021 <sbaek18@gmail.com>
2021-09-05 15:43:44 +0200

    -Added an extra Coffe Class  WhiteFlat -Added it to the list -Printed it out to the console

--- CoffeeShopConsoleApp/Program.cs
+++ CoffeeShopConsoleApp/Program.cs
@@ -8,14 +8,15 @@ namespace CoffeeShopConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Coffee Shop!");
-
+            Console.WriteLine("---------------------------------");
            // a list of all the orders for coffe in the coffee shop
 
            List < Coffee > orderList = new List<Coffee>()
            {
                 new Latte(1),
                 new Cortado(1),
-                new BlackCoffee(1)
+                new BlackCoffee(1),
+                new FlatWhite(0)
 
            };
 
@@ -32,11 +33,12 @@ namespace CoffeeShopConsoleApp
             List<Coffee> milkyCoffee = new List<Coffee>()
             {
                 new Latte(0),
-                new Cortado(1)
+                new Cortado(0)
 
             };
            
             Console.WriteLine("Coffee with Milk");
+            Console.WriteLine("---------------------------");
             foreach (Coffee coffee in milkyCoffee)
             {
                 Console.WriteLine("the Price of the coffee is = {0}, Strength= {1} ", coffee.price(), coffee.Strength());
