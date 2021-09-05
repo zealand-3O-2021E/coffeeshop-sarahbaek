db1cafe2a64ec01257dc0b6f831c9bec9e1779df
sarahBæk2021 <sbaek18@gmail.com>
2021-09-05 16:02:43 +0200

    ..

---
+++ CoffeeShopConsoleApp/FlatWhite.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeShopConsoleApp
+{
+    public class FlatWhite : Coffee
+    {
+        public FlatWhite(int discount): base(discount)
+        {
+
+        }
+        public override string Strength()
+        {
+            Console.WriteLine("Flat White");
+            Console.WriteLine("___________________");
+            return "Weak";
+        }
+    }
+}
