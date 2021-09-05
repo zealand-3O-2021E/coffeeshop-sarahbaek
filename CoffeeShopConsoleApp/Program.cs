06dc9a4672761f140b1fcdd9aff13b3f35676231
sarahBæk2021 <sbaek18@gmail.com>
2021-09-05 14:48:30 +0200

    -Printed out the coffee types , prices and strengths to the console

--- CoffeeShopConsoleApp/Program.cs
+++ CoffeeShopConsoleApp/Program.cs
@@ -9,21 +9,29 @@ namespace CoffeeShopConsoleApp
         {
             Console.WriteLine("Hello Coffee Shop!");
 
-            // a list of all the orders for coffe in the coffee shop
-            //List<Coffee> orderList = new List<Coffee>()
-            //{
-            //    new Coffee(),
-            //    new Coffee(),
-            //    new Coffee()
+           // a list of all the orders for coffe in the coffee shop
 
-            //};
+           List < Coffee > orderList = new List<Coffee>()
+           {
+                new Latte(),
+                new Cortado(),
+                new BlackCoffee()
+
+           };
 
 
-            //foreach (var coffee in orderList)
-            //{
-            //    Console.WriteLine("the Price of the coffee is : "+ coffee.price());
-            //}
+            foreach (var coffee in orderList)
+            {
+                Console.WriteLine("the Price of the coffee is = {0}, Strength= {1} ", coffee.price(), coffee.Strength());
+                //Console.WriteLine("The proce of the coffe is = "coffee.price);
+            }
 
+            //Latte l1 = new Latte();
+            //Cortado c1 = new Cortado();
+            //BlackCoffee bc = new BlackCoffee();
+            ////List<Coffee> cc = new Latte();
+
+            Console.ReadLine();
 
         }
     }
